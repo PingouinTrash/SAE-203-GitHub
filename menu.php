995c82f1b75c05d3718e20c89e6ca334868c057b
Shleongberg <r3d32005@gmail.com>
2024-05-21 15:12:33 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,21 +1,20 @@
-<nav class="main-navbar">
-            <div>
-                <a class="home-link" href="#">
-                    <img class="img-64px logo" src="" alt="" />
-                </a>
+<nav>
+    <div>
+        <a  href="#">
+            <img class="img-64px logo" src="" alt="" />
+        </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <nav>
-                    <ul>
-                        <li><a href="#">Catalogue</a></li>
-                        <li><a href="#">Magasin</a></li>
-                        <li><a href="#">stock</a></li>
-                    </ul>
-                    <hr class="menu-sep">
-                    <a class="contact-button" href="#">Contact</a>
-                </nav>
-            </div>
-        </nav>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div>
+            <ul>
+                <li><a href="#">Catalogue</a></li>
+                <li><a href="#">Magasin</a></li>
+                <li><a href="#">stock</a></li>
+            </ul>
+            <hr class="menu-sep">
+            <a class="contact-button" href="#">Contact</a>
+        </div>
+    </div>
+</nav>
