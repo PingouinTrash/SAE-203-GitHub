82c689b5187923de576bd57921fc2402988b9923
PingouinTrash <antonin.pierre.thomas@orange.fr>
2024-05-31 17:05:17 +0200

    Update create_databases.sql

--- create_databases.sql
+++ create_databases.sql
@@ -1,17 +1,3 @@
-CREATE TABLE Bonbons (
-    bonbon_id SERIAL PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    prix_unitaire FLOAT
-);
-
-CREATE TABLE Stocks ();
-
-CREATE TABLE Boutiques ();
-
-CREATE TABLE Utilisateurs ();
-
-
 CREATE TABLE utilisateurs (
     utilisateur_id INT PRIMARY KEY,
     nom VARCHAR(50),
