d17010028555c33dd31835791c16699801b2d022
Shleongberg <r3d32005@gmail.com>
2024-06-11 10:41:27 +0200

    d

--- boutique.php
+++ boutique.php
@@ -15,6 +15,28 @@ include_once("menu.php");
             </h3>
         </div>
     </div>
+    <div class="Boutique">
+        <img src="media/Boutique.jpg" alt="Boutique de ...">
+        <div>
+            <h2>
+                Nom de la Boutique
+            </h2>
+            <h3>
+                Adresse de la Boutique
+            </h3>
+        </div>
+    </div>
+    <div class="Boutique">
+        <img src="media/Boutique.jpg" alt="Boutique de ...">
+        <div>
+            <h2>
+                Nom de la Boutique
+            </h2>
+            <h3>
+                Adresse de la Boutique
+            </h3>
+        </div>
+    </div>
 </section>
 
 
