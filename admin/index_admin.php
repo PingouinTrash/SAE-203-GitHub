1214215f8b8603e5cc45c78ce2515f645c124eea
PingouinTrash <antonin.pierre.thomas@orange.fr>
2024-06-19 21:28:10 +0200

    Ajout et suppression de boutique fonctionnel

--- admin/index_admin.php
+++ admin/index_admin.php
@@ -8,89 +8,93 @@ if ($_SESSION["role"] != "admin") {
     header("location: " . ROOT . "index.php");
     exit();
 }
-
-if (isset($_POST["actiontype"])) {
-
-    if($_POST["actiontype"] == "ajout_boutique"){
-        $nomBoutique = $_POST['nom-boutique'];
-        $gerant = $_POST['gerant'];
-        $numRue = $_POST['num-rue'];
-        $nomRue = $_POST['nom-adresse'];
-        $codePostal = $_POST['code-postal'];
-        $ville = $_POST['ville'];
-        $pays = $_POST['pays'];
-
-        $stmt = $bdd->prepare("INSERT INTO boutiques (id, nom, utilisateur_id, numero_rue, nom_adresse, code_postal, ville, pays) VALUES (NULL, :nom, :utilisateur_id, :numero_rue, :nom_adresse, :code_postal, :ville, :pays)");
-        $stmt->execute([
-            "nom" => $nomBoutique,
-            "utilisateur_id" => $gerant,
-            "numero_rue" => $numRue,
-            "nom_adresse" => $nomRue,
-            "code_postal" => $codePostal,
-            "ville" => $ville,
-            "pays" => $pays
-        ]);
-
-        header("Location: index_admin.php");
-        exit();
-    }
-
-    elseif($_POST["actiontype"] == "suppression_boutique"){
-        header("Location: index_admin.php");
-        exit();
-    }
-}
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>Gestion des boutiques</title>
-</head>
-<body>
-  <section>
+<section>
     <div class="nom-de-page">
         <h2>Gestion des boutiques</h2>
     </div>
-  </section>
+</section>
 
-  <form method="post">
-    <input type="hidden" name="actiontype" value="ajout_boutique">
+<section class="liste">
+    <form method="post">
+        <label for="nom-boutique">Nom de la boutique :</label>
+        <input type="text" id="nom-boutique" name="nom-boutique" placeholder="Nom Boutique ..." required><br>
 
-    <label for="nom-boutique">Nom de la boutique :</label>
-    <input type="text" id="nom-boutique" name="nom-boutique" placeholder="Nom Boutique ..." required><br>
+        <label for="gerant">id du gérant :</label>
+        <input type="number" id="gerant" name="gerant" placeholder="gérant id ..." required><br>
 
-    <label for="gerant">id du gérant :</label>
-    <input type="number" id="gerant" name="gerant" placeholder="gérant id ..." required><br>
+        <label for="num-rue">Numéro de la rue :</label>
+        <input type="number" id="num-rue" name="num-rue" placeholder="Numéro rue ..." required><br>
 
-    <label for="num-rue">Numéro de la rue :</label>
-    <input type="number" id="num-rue" name="num-rue" placeholder="Numéro rue ..." required><br>
+        <label for="nom-adresse">Nom de l'adresse :</label>
+        <input type="text" id="nom-adresse" name="nom-adresse" placeholder="Nom adresse ..." required><br>
 
-    <label for="nom-adresse">Nom de l'adresse :</label>
-    <input type="text" id="nom-adresse" name="nom-adresse" placeholder="Nom adresse ..." required><br>
+        <label for="code-postal">code postal :</label>
+        <input type="number" id="code-postal" name="code-postal" placeholder="code postal ..." required><br>
 
-    <label for="code-postal">code postal :</label>
-    <input type="number" id="code-postal" name="code-postal" placeholder="code postal ..." required><br>
+        <label for="ville">ville :</label>
+        <input type="text" id="ville" name="ville" placeholder="ville ..." required><br>
 
-    <label for="ville">ville :</label>
-    <input type="text" id="ville" name="ville" placeholder="ville ..." required><br>
+        <label for="pays">Pays :</label>
+        <input type="text" id="pays" name="pays" placeholder="Pays ..." required><br>
 
-    <label for="pays">Pays :</label>
-    <input type="text" id="pays" name="pays" placeholder="Pays ..." required><br>
-
-    <input type="submit" value="Créer la boutique" name="Boutique-créé">
-  </form>
-
-  <section id="liste-boutiques" class="liste">
+        <input type="hidden" name="actiontype" value="ajout_boutique">
+        <button type="submit" class="bouton fond-clair"><h4>Créer une boutique</h4></button>
+    </form>
+</section>
+  
+<section id="liste-boutiques" class="liste">
     <div>
         <?php
+
+        if (isset($_POST["actiontype"])) {
+
+            if($_POST["actiontype"] == "ajout_boutique"){
+                $nomBoutique = $_POST['nom-boutique'];
+                $gerant = $_POST['gerant'];
+                $numRue = $_POST['num-rue'];
+                $nomRue = $_POST['nom-adresse'];
+                $codePostal = $_POST['code-postal'];
+                $ville = $_POST['ville'];
+                $pays = $_POST['pays'];
+
+                $stmt = $bdd->prepare("INSERT INTO boutiques (id, nom, utilisateur_id, numero_rue, nom_adresse, code_postal, ville, pays) VALUES (NULL, :nom, :utilisateur_id, :numero_rue, :nom_adresse, :code_postal, :ville, :pays)");
+                $stmt->execute([
+                    "nom" => $nomBoutique,
+                    "utilisateur_id" => $gerant,
+                    "numero_rue" => $numRue,
+                    "nom_adresse" => $nomRue,
+                    "code_postal" => $codePostal,
+                    "ville" => $ville,
+                    "pays" => $pays
+                ]);
+
+                header("Location: index_admin.php");
+                exit();
+            }
+
+            elseif($_POST["actiontype"] == "suppression_boutique"){
+                $boutique_id = $_POST['boutique_id'];
+
+                print_r('<pre>');
+                print_r($boutique_id);
+                print_r('</pre>');
+
+                $request_boutique = "DELETE FROM boutiques WHERE id = $boutique_id";
+                $suppr_boutique = query($request_boutique);
+
+                header("Location: index_admin.php");
+                exit();
+            }
+        }
+
         $resultat = $bdd->query("SELECT * FROM boutiques");
 
         if ($resultat && $resultat->rowCount() > 0) {
             while ($row = $resultat->fetch(PDO::FETCH_ASSOC)) {
                 echo (
-                    "
+                    "<a href='index_admin'>
                         <div class='card horizontal'>
                             <img class='image' src='../media/Boutique.jpg' alt='Boutique de " . $row["nom"] . "'>
                             <div class='desc'>
@@ -99,9 +103,11 @@ if (isset($_POST["actiontype"])) {
                             </div>
                             <form method='post'>
                                 <input type='hidden' name='actiontype' value='suppression_boutique'>
-                                <button type='button'>Supprimer la boutique</button>
+                                <input type='hidden' name='boutique_id' value=" . $row["id"] . ">
+                                <button type='submit' class='bouton fond-sombre'><h4>Supprimer la boutique</h4></button>
                             </form>
-                        </div>"
+                        </div>
+                    </a>"
                 );
             }
         } else {
@@ -109,7 +115,7 @@ if (isset($_POST["actiontype"])) {
         }
         ?>
     </div>
-  </section>
+</section>
 
   <?php
   include_once(ROOT . "footer.php");
